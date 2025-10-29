4f07ffd0ac41e13bf3ff5aef1fd34ed680127a57
Mansi Unge <mansiunge@gmail.com>
2025-10-29 14:59:37 +0530

    program_08 done

---
+++ Data_types/Program_08.py
@@ -0,0 +1,37 @@
+"""
+Concept: Key-value mapping
+
+Question:
+Create dictionary:
+student = {'name': 'Mansi', 'age': 20, 'marks': 85}
+Print all keys, values, and items.
+
+Steps:
+Use .keys(), .values(), .items()
+
+Output:
+dict_keys(['name','age','marks'])
+dict_values(['Mansi',20,85])
+dict_items([('name','Mansi'),('age',20),('marks',85)])
+"""
+
+# CODE :
+
+#dictionary of student data  
+student = {
+  'name':'mansi' ,
+  'age' : '22' ,
+  'marks' : '85'
+}
+
+# getting keys in dictionary
+dict_keys = student.keys()
+print("keys in student dictionary are :", dict_keys)
+
+# getting values in dictionary 
+dict_values = student.values()
+print("values in student dictionary are :", dict_values)
+
+# getting the dictionary items 
+dict_items = student.items()
+print("items in student dictionary are :",dict_items)
