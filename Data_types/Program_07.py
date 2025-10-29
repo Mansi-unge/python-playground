27775f2027da8e4ef68d85d08a9dd170490ff391
Mansi Unge <mansiunge@gmail.com>
2025-10-29 14:16:47 +0530

    program_07 done

--- Data_types/Program_07.py
+++ Data_types/Program_07.py
@@ -10,3 +10,16 @@ Output:
 """
 
 # CODE 
+num = (10,20,30)
+a,b,c = num 
+print("value of a in tuple is:",a)
+print("value of b in tuple is:",b)
+print("value of c in tuple is:",c)
+
+
+"""
+OUTPUT :
+value of a in tuple is: 10
+value of b in tuple is: 20
+value of c in tuple is: 30
+"""
