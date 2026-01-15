cc3b9813c8bf396fb1c7f1bafc9a2c4405f8b668
Mansi Unge <mansiunge@gmail.com>
2026-01-15 10:20:54 +0530

    boolean and logical operator

--- Boolean_logical_operator/program_10.py
+++ Boolean_logical_operator/program_10.py
@@ -2,3 +2,10 @@
 
 # Check whether a person is eligible to vote
 # (Condition: age ≥ 18)
+    
+
+age = 13
+if age>=18:
+    print("You are ELIGIBLE to vote")
+else:
+    print("you are NOT eligible to vote")
--- Boolean_logical_operator/program_11.py
+++ Boolean_logical_operator/program_11.py
@@ -3,3 +3,14 @@
 # Takes username and password
 
 # Prints Login Successful if both are correct, else Login Failed
+
+username = "mansi"
+password = "mansi@123"
+
+user_name = input("Enter your Username : ")
+pass_word = input("Enter your Password : ")
+
+if username == user_name and password == pass_word:
+  print("Login Successfull.....!")
+else:
+  print("Login Failed")
