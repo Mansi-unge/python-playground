dcdde25fd6698185c6618d71a7a178e397a6edd8
Mansi Unge <mansiunge@gmail.com>
2026-01-16 20:33:49 +0530

    mutability and immutability

--- Mutability_and_Immutability/program_15.py
+++ Mutability_and_Immutability/program_15.py
@@ -1,3 +1,14 @@
 # Write a program to:
 
 # Modify a list inside a function and observe changes outside the function
+
+def change_list(num):
+  num.append(10)
+  print("list inside function",num)
+
+
+num = [2,4,6,8]
+print("list outside function before function call : ",num)
+
+change_list(num)
+print("list outside the function after the function call :",num)
