a0486c3e465d9f8910674cff3ba701e62046d67c
Mansi Unge <mansiunge@gmail.com>
2026-01-16 20:54:29 +0530

    mutability and immutability

--- Mutability_and_Immutability/program_16.py
+++ Mutability_and_Immutability/program_16.py
@@ -1,3 +1,16 @@
-# Write a program to:
+# Program to compare two lists using == and is operators
 
-# Compare two lists using == and is operators
+num1 = [2, 3, 4, 5]
+num2 = [2, 3, 4, 5]
+
+# Using == operator (checks values)
+if num1 == num2:
+    print("num1 and num2 have same values (==)")
+else:
+    print("num1 and num2 have different values (==)")
+
+# Using is operator (checks memory location)
+if num1 is num2:
+    print("num1 and num2 refer to the same object (is)")
+else:
+    print("num1 and num2 refer to different objects (is)")
--- Mutability_and_Immutability/program_17.py
+++ Mutability_and_Immutability/program_17.py
@@ -2,3 +2,8 @@
 
 # Show that integers are immutable using id()
 
+num = 1 
+print("id of num before changing the value : ",id(num))
+
+num = 2
+print("id of num after changing the value : ",id(num))
