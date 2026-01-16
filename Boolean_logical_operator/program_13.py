8a4f607e4626b47d34d18bf9731c24d0ecf70b27
Mansi Unge <mansiunge@gmail.com>
2026-01-16 18:31:11 +0530

    boolean and logical operator

--- Boolean_logical_operator/program_13.py
+++ Boolean_logical_operator/program_13.py
@@ -1,3 +1,10 @@
 # Write a program to:
 
 # Check if a year is a leap year using logical operators
+
+year = 2000
+
+if (year%4 == 0 and year%100 !=0) or year % 400 == 0:
+  print("year is leap year")
+else:
+  print("year is not leap year")
