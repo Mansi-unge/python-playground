3f2a602a435dcf814c5618b2f1a2c02ad2135045
Mansi Unge <mansiunge@gmail.com>
2026-01-17 21:43:29 +0530

    learning string data type

--- String/STRING_COMPARISON_AND_MEMBERSHIP/program_45.py
+++ String/STRING_COMPARISON_AND_MEMBERSHIP/program_45.py
@@ -1,1 +1,9 @@
 # Write a program to check whether two strings are equal ignoring case.
+
+str1 = "String"
+str2 = "string"
+
+if str1.lower() == str2.lower():
+  print("Both Strings are equal")
+else:
+  print("both strings are diffrent")
--- String/STRING_COMPARISON_AND_MEMBERSHIP/program_46.py
+++ String/STRING_COMPARISON_AND_MEMBERSHIP/program_46.py
@@ -1,1 +1,5 @@
 # Write a program to sort characters of a string alphabetically.
+
+string = "mansi"
+sorted_String = " ".join(sorted(string))
+print(f"string {string} after sorting looks like - {sorted_String}")
