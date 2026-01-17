8844bd54677921c9c2ef9f1a75a17bfdbff61200
Mansi Unge <mansiunge@gmail.com>
2026-01-17 18:03:21 +0530

    learning string data type

--- String/STRING_METHODS/program_37.py
+++ String/STRING_METHODS/program_37.py
@@ -1,2 +1,7 @@
 # Write a program to find the position of a substring in a string.
 
+string = "program to find the position of a substring in a string."
+
+
+substring_position = string.find("position")
+print(substring_position)
