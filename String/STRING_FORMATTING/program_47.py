e3d30b292abb12e78339a181e3881cd93dd6b0dc
Mansi Unge <mansiunge@gmail.com>
2026-01-17 21:48:08 +0530

    learning string data type

--- String/STRING_FORMATTING/program_47.py
+++ String/STRING_FORMATTING/program_47.py
@@ -1,3 +1,13 @@
 # Write a program to display student details using:
 # • format() method
 # • f-string
+
+name = "mansi sanjay unge"
+course = "MCA"
+year = "first year"
+
+# format() method
+print("{} is the student of {} department of {}".format(name,course,year))
+
+# f-string method
+print(f"{name} is the student of {course} department of {year}")
