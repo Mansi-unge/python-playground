7301565c82366c9e9833e3224ef1f7ab292c3e28
Mansi Unge <mansiunge@gmail.com>
2026-01-17 10:51:37 +0530

    udemy excercise questions

---
+++ Udemy_excercise/problem_1.py
@@ -0,0 +1,27 @@
+# You’re deciding whether to go for a walk based on two real-life conditions:
+
+# is_sunny = True
+
+# have_umbrella = False
+
+
+
+# Print the result of the following:
+
+# Is it not sunny today?
+
+# Do you not have an umbrella?
+
+# Should you go for a walk if it’s sunny and you don’t need an umbrella?
+
+# Should you go for a walk if it’s sunny or if you have an umbrella?
+
+is_sunny = True
+have_umbrella = False
+
+print("Is it not sunny today?", not is_sunny)
+print("Do you not have an umbrella?", not have_umbrella)
+
+print("Should you go for a walk if it’s sunny and you don’t need an umbrella?",is_sunny and not have_umbrella)
+
+print("Should you go for a walk if it’s sunny or if you have an umbrella?",is_sunny or have_umbrella)
