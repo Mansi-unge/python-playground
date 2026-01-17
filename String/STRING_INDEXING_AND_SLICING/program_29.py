2f29c7f4fc9801d11f32b76bf3f42e28828af59b
Mansi Unge <mansiunge@gmail.com>
2026-01-17 15:51:31 +0530

    learning string data type

--- String/STRING_INDEXING_AND_SLICING/program_29.py
+++ String/STRING_INDEXING_AND_SLICING/program_29.py
@@ -1,1 +1,6 @@
 # Write a program to print alternate characters of a string.
+
+name = "mansi unge"
+print("original string : ",name)
+alternate_char = name[0::2]
+print("alternate charachters of string : ",alternate_char)
--- String/STRING_INDEXING_AND_SLICING/program_30.py
+++ String/STRING_INDEXING_AND_SLICING/program_30.py
@@ -1,1 +1,9 @@
 # Write a program to check whether a string is a palindrome.
+
+string = "mansi"
+string2 = string[::-1]
+
+if string == string2:
+  print("string is palindrome")
+else:
+  print("string is not palindrome")
