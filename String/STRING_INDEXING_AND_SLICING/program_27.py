6e48a6290c3c5dc1a24a0f77d5c59b6d54ef9294
Mansi Unge <mansiunge@gmail.com>
2026-01-17 15:34:44 +0530

    learning string data type

--- String/STRING_INDEXING_AND_SLICING/program_27.py
+++ String/STRING_INDEXING_AND_SLICING/program_27.py
@@ -1,1 +1,5 @@
 # Write a program to reverse a string using slicing.
+
+name = "mansi"
+reverse_String = name[::-1]
+print("reversed string is : ",reverse_String)
--- String/STRING_INDEXING_AND_SLICING/program_28.py
+++ String/STRING_INDEXING_AND_SLICING/program_28.py
@@ -1,1 +1,11 @@
 # Write a program to extract first half and second half of a string.
+
+string = "I am extracting the first half and second half of a string seperately"
+print("original string")
+print(string)
+
+middle_index = len(string)//2
+first_half = string[0:middle_index]
+second_half = string[middle_index:]
+print("first half of the string is :",first_half)
+print("Second half of the string is :",second_half)
