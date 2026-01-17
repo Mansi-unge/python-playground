9c50f6798c307bef7e84dc65b79e45e0eab8996d
Mansi Unge <mansiunge@gmail.com>
2026-01-17 14:48:02 +0530

    learning string data type

--- String/BASIC_STRING_OPERATIONS/program_24.py
+++ String/BASIC_STRING_OPERATIONS/program_24.py
@@ -1,1 +1,7 @@
 # Write a program to concatenate two strings without using + operator.
+
+f_name = "mansi"
+l_name = "unge"
+
+name = " ".join([f_name,l_name])
+print("concatenated string without using string operator is : ",name)
