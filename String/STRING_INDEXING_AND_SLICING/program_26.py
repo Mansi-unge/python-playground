b02cf04046092cca0d4447c44d0fa61c24b8e2f8
Mansi Unge <mansiunge@gmail.com>
2026-01-17 15:00:36 +0530

    learning string data type

--- String/STRING_INDEXING_AND_SLICING/program_26.py
+++ String/STRING_INDEXING_AND_SLICING/program_26.py
@@ -2,3 +2,14 @@
 # • First character
 # • Last character
 # • Middle character of a string
+
+
+name = "mansi"
+middle_index = len(name)//2
+first_char = name[0]
+last_char = name[-1]
+middle_char = name[middle_index]
+
+print(first_char)
+print(last_char)
+print(middle_char)
