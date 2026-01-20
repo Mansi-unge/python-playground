ebd1acc0038e7fabdf035fa001ac6054d4c3a3f8
Mansi Unge <mansiunge@gmail.com>
2026-01-20 20:49:20 +0530

    udemy excercise questions

---
+++ Udemy_excercise/problem_2.py
@@ -0,0 +1,21 @@
+# Swapping Temperature
+# You are building a temperature converter app. Sometimes, due to incorrect input order, the min_temp and max_temp values are swapped.
+
+# Current values are
+
+# min_temp = 40
+
+# max_temp = 25
+
+
+
+# Use Python tuples to swap them back to the correct order.
+
+min_temp = 40
+max_temp = 25
+
+
+# Write your code here
+min_temp , max_temp = 25,40
+print(min_temp)
+print(max_temp)
