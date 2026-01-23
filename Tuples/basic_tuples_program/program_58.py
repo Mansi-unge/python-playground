3fdafbbfaae61e858a3a7df11fa16eac835a8d69
Mansi Unge <mansiunge@gmail.com>
2026-01-23 07:38:37 +0530

    Learning Tuples in Python

---
+++ Tuples/basic_tuples_program/program_58.py
@@ -0,0 +1,9 @@
+# Write a program to create a tuple and display its elements.
+
+tup = ("apple","cherry ", "banana","mango")
+print("Display all elements at once")
+print(tup)
+
+print("\n Display all elements one by one : ")
+for element in tup:
+  print(element)
---
+++ Tuples/basic_tuples_program/program_59.py
@@ -0,0 +1,5 @@
+# Write a program to create a tuple with different data types.
+
+tup = (1,"Mansi",5.2,"U",True)
+print("Tuple with diffrent datatypes : ")
+print(tup)
