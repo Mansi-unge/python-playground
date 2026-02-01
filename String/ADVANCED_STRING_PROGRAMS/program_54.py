364a147c977a9d820c2447995517866aa4deeb53
Mansi Unge <mansiunge@gmail.com>
2026-02-01 11:30:26 +0530

    learning string data type

--- String/ADVANCED_STRING_PROGRAMS/program_54.py
+++ String/ADVANCED_STRING_PROGRAMS/program_54.py
@@ -1,2 +1,16 @@
 # Write a program to compress a string
 # Example: aaabb → a3b2
+
+string = "aaabbbbb"
+compressed = ""
+count = 1
+
+for i in range(1,len(string)):
+  if string[i] == string[i-1]:
+    count+=1
+  else:
+    compressed += string[i-1]+str(count)
+    count = 1
+compressed += string[i-1]+str(count)
+print("string compressed is : ",compressed)
+  
--- String/ADVANCED_STRING_PROGRAMS/program_55.py
+++ String/ADVANCED_STRING_PROGRAMS/program_55.py
@@ -1,1 +1,2 @@
 # Write a program to convert a string into title case without using title().
+
