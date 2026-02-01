2d3b5331bd0f0be8de383161c484c1b04a348dd9
Mansi Unge <mansiunge@gmail.com>
2026-02-01 11:22:22 +0530

    learning string data type

--- String/ADVANCED_STRING_PROGRAMS/program_53.py
+++ String/ADVANCED_STRING_PROGRAMS/program_53.py
@@ -1,1 +1,9 @@
 # Write a program to check whether two strings are anagrams.
+
+word_1 = "listen  "
+word_2 = "silent"
+
+if sorted(word_1) == sorted(word_2):
+  print("the given strings are anagrams")
+else:
+  print("given strings are not anagrams")
--- String/STRING_AND_LOOPS/program_52.py
+++ String/STRING_AND_LOOPS/program_52.py
@@ -1,1 +1,15 @@
 # Write a program to find the longest word in a sentence.
+
+sentence = "program to find the longest word in a sentence."
+
+
+words = sentence.split()
+print(words)
+
+longest_word = words[0]
+
+for word in words:
+  if len(word) > len(longest_word):
+    longest_word = word
+
+print("longest word : ",longest_word)
