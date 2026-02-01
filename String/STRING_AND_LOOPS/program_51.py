3c7fa8ced670e7f36efb5e408d22c060e61d493b
Mansi Unge <mansiunge@gmail.com>
2026-02-01 11:11:04 +0530

    learning string data type

--- String/STRING_AND_LOOPS/program_51.py
+++ String/STRING_AND_LOOPS/program_51.py
@@ -1,1 +1,9 @@
 # Write a program to remove duplicate characters from a string.
+
+my_string = "frequency"
+result = ""
+for char in my_string:
+  if char not in result:
+    result += char
+
+print(result)
