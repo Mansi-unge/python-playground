6e3100058b5bab637aed7edfe10da82aa4458e6e
Mansi Unge <mansiunge@gmail.com>
2026-02-01 11:03:52 +0530

    learning string data type

--- String/STRING_AND_LOOPS/program_50.py
+++ String/STRING_AND_LOOPS/program_50.py
@@ -1,2 +1,13 @@
 # Write a program to find the frequency of each character in a string.
 
+my_string = "frequency"
+
+
+
+for char in my_string:
+  freq = 0
+  for ch in my_string:
+    if ch == char:
+      freq += 1
+  print(char , " : " , freq)  
+
