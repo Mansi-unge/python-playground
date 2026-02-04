355f011bf79b4de45c34c41f447a0f113eee3209
Mansi Unge <mansiunge@gmail.com>
2026-02-04 06:30:02 +0530

    udemy excercise questions

--- Boolean_logical_operator/program_13.py
+++ Boolean_logical_operator/program_13.py
@@ -1,10 +1,10 @@
-# Write a program to:
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year % 4 == 0  and year % 100 != 0) or year % 400 == 0:
+        return year
+    return leap
 
-# Check if a year is a leap year using logical operators
-
-year = 2000
-
-if (year%4 == 0 and year%100 !=0) or year % 400 == 0:
-  print("year is leap year")
-else:
-  print("year is not leap year")
+year = int(input("enter your year: "))
+print(is_leap(year))
